ced91a81d2bea10acbf2c64e42151bbd1278c1bf
susanna777mato <susanna.mato777@gmail.com>
2024-09-15 06:55:49 -0700

    i add corectit

--- src/app/component/atwork/atwork.component.ts
+++ src/app/component/atwork/atwork.component.ts
@@ -24,27 +24,28 @@ export class AtworkComponent implements OnInit {
   displayValue: string = ""
  
     resultFunction() {
-     this.yourMany = (this.inputRange1 * (this.inputRange2 / 100) )
+     this.yourMany = Math.round(this.inputRange1 * (this.inputRange2 / 100))
    
      if (+this.inputRange1 > 900000) {
        this.inputRange1 = 900000
-       this.yourMany = 900000 * (this.inputRange2 / 100)
+      this.yourMany =  Math.round( 900000 * (this.inputRange2 / 100))
      }
   
      if (+this.inputRange1 < 1000) {
        this.inputRange1 = 1000
-       this.yourMany = 1000 * (this.inputRange2 / 100)
+       this.yourMany = Math.round(this.inputRange1 * (this.inputRange2 / 100))
+
      }
  
  
      if (+this.inputRange2 > 20) {
        this.inputRange2 = 20
-       this.yourMany = this.inputRange1 * (20 / 100)
+       this.yourMany = Math.round( this.inputRange1 * (20 / 100))
      }
  
      if (+this.inputRange2 < 5) {
        this.inputRange2 = 5
-       this.yourMany = this.inputRange1 * (5 / 100)
+       this.yourMany = Math.round( this.inputRange1 * (5 / 100)) 
      }
  
     }
